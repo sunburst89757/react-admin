0aaae4cfb517cf4d84c12fc857c07a68e083dbc6
sunburst89757 <3520279278@qq.com>
2023-01-06 16:04:10 +0800

    fix(lazyload): 修复路由懒加载在vite上构建无法识别的bug

--- src/router/utils/lazyLoad.tsx
+++ src/router/utils/lazyLoad.tsx
@@ -1,7 +1,16 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, ComponentType } from "react";
 // path是文件夹的路径
 export function LazyLoad({ path }: { path: string }) {
-  const Component = lazy(() => import(`../../pages/${path}`));
+  const modules = import.meta.glob<
+    boolean,
+    string,
+    { default: ComponentType<any> }
+  >("../../pages/**/index.tsx");
+  const key = Object.keys(modules).find(
+    (moduleName) => moduleName.indexOf(path) > 0
+  );
+  if (!key) return null;
+  const Component = lazy(modules[key]);
   return (
     <Suspense fallback={<>加载中……</>}>
       <Component></Component>
